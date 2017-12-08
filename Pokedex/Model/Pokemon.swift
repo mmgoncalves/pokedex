cc8dc0bdd4e18efcaf7db95e4573a1b38c7013e6
Mateus Marques <marques.m05@gmail.com>
2017-12-08 19:49:31 -0200

    create layout

---
+++ Pokedex/Model/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Mateus Marques on 07/12/17.
+//  Copyright © 2017 Mateus Marques. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
